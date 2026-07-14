c852f0266861ffb585da97d605f2c9778e8c6511
JoaoMarcelo-Faria <joaogcfaria@gmail.com>
2026-07-14 18:45:22 -0300

    chore: ajustes no schema de resposta e listagem

--- mycarapi/schemas/users.py
+++ mycarapi/schemas/users.py
@@ -1,21 +1,30 @@
+from datetime import datetime
 from typing import Optional, List
 
-from pydantic import BaseModel, EmailStr
+from pydantic import BaseModel, ConfigDict, EmailStr
 
 class UserRequestSchema(BaseModel):
     username: str
     email: EmailStr
     password: str
 
+
 class UserResponseSchema(BaseModel):
+    model_config = ConfigDict(from_attributes=True)     # permite a criação dessa classe a partir de objetos diferentes de um dict
     id: int
     username: str
     email: EmailStr
+    created_at: datetime
+    updated_at: datetime
+
 
 class UpdateUserSchema(BaseModel):
     username: Optional[str] = None
     email: Optional[EmailStr] = None
     password: Optional[str] = None
 
+
 class ListUserSchema(BaseModel):
     users: List[UserResponseSchema]
+    offset: int
+    limit: int
